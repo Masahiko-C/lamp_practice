78fa9a0acccd8d4ab78b4f0c4efce406aa166e6f
Masahiko Naruse <kirakiramasahiko@gmail.com>
2020-11-11 17:56:12 +0900

    CSRF対応

--- www/html/admin_insert_item.php
+++ www/html/admin_insert_item.php
@@ -24,11 +24,14 @@ $status = get_post('status');
 $stock = get_post('stock');
 
 $image = get_file('image');
+$token = get_post('token');
 
-if(regist_item($db, $name, $price, $stock, $status, $image)){
-  set_message('商品を登録しました。');
-}else {
-  set_error('商品の登録に失敗しました。');
+if(is_valid_csrf_token($token)){
+  if(regist_item($db, $name, $price, $stock, $status, $image)){
+    set_message('商品を登録しました。');
+  }else {
+    set_error('商品の登録に失敗しました。');
+  }
 }
 
 
